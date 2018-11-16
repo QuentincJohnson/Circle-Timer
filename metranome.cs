5006591c2379231bead72ac129b50f6e377f1940
QuentincJohnson <45111313+QuentincJohnson@users.noreply.github.com>
2018-11-16 18:36:19 -0500

    Add files via upload

---
+++ metranome.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class sound  {
+
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+
+}
